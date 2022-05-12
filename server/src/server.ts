7e3cd81dc90531fcdf717589c1a3d77e2e27922e
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-05-12 17:51:56 +0300

    payload too large

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,8 @@ const app: express.Application = express();
 const port: string|number = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 app.use(express.static('../../client/build'))
 
 app.use('/graphql', graphqlHTTP({
